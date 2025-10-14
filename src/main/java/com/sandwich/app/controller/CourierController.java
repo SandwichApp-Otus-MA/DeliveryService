f0e28e0c7df1093041ded10fb3c3420a835d54af
AnVoronov <AnVoronov@croc.ru>
2025-10-14 15:46:35 +0300

    добавлен базовые возможности для курьера

--- src/main/java/com/sandwich/app/controller/CourierController.java
+++ src/main/java/com/sandwich/app/controller/CourierController.java
@@ -1,14 +1,17 @@
 package com.sandwich.app.controller;
 
 import com.sandwich.app.models.model.delivery.courier.CourierDto;
+import com.sandwich.app.models.model.enums.CourierStatus;
 import com.sandwich.app.service.CourierService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -24,5 +27,11 @@ public class CourierController {
     @PostMapping("/create")
     public ResponseEntity<UUID> create(@Valid @RequestBody CourierDto courier) {
         return ResponseEntity.ok(service.create(courier));
+    }
+
+    @PostMapping("/change-status/{courierId}")
+    public ResponseEntity<Void> changeStatus(@PathVariable UUID courierId, @RequestParam CourierStatus status) {
+        service.changeStatus(courierId, status);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/sandwich/app/service/CourierService.java
+++ src/main/java/com/sandwich/app/service/CourierService.java
@@ -4,14 +4,22 @@ import com.sandwich.app.domain.entity.CourierEntity;
 import com.sandwich.app.domain.entity.DeliveryEntity;
 import com.sandwich.app.domain.repository.CourierRepository;
 import com.sandwich.app.mapper.CourierMapper;
+import com.sandwich.app.models.model.delivery.DeliveryDto;
+import com.sandwich.app.models.model.delivery.DeliveryFilter;
 import com.sandwich.app.models.model.delivery.courier.CourierDto;
+import com.sandwich.app.models.model.enums.AdvancedFieldFilterType;
 import com.sandwich.app.models.model.enums.CourierStatus;
 import com.sandwich.app.models.model.enums.DeliveryStatus;
+import com.sandwich.app.models.pagination.AdvancedFieldFilter;
+import com.sandwich.app.models.pagination.Pagination;
+import com.sandwich.app.models.pagination.PaginationRequest;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
@@ -57,5 +65,31 @@ public class CourierService {
                     //  иначе отменяем заказ
                 }
             );
+    }
+
+    @Transactional
+    public void changeStatus(UUID courierId, CourierStatus status) {
+        var delivery = getFirstActiveDelivery(courierId);
+
+        if (status == CourierStatus.OFFLINE && !delivery.isEmpty()) {
+            throw new IllegalArgumentException("Требуется завершить активную доставку!");
+        }
+
+        repository.findById(courierId)
+            .orElseThrow(() -> new EntityNotFoundException("Courier  not found!"))
+            .setStatus(status);
+    }
+
+    private List<DeliveryDto> getFirstActiveDelivery(UUID courierId) {
+        var request = new PaginationRequest<DeliveryFilter>()
+            .setFilter(new DeliveryFilter()
+                .setCourierId(new AdvancedFieldFilter<UUID>()
+                    .setType(AdvancedFieldFilterType.EQUALS)
+                    .setSingleValue(courierId))
+                .setStatus(new AdvancedFieldFilter<DeliveryStatus>()
+                    .setType(AdvancedFieldFilterType.IN)
+                    .setMultipleValue(List.of(DeliveryStatus.COURIER_TO_RESTAURANT, DeliveryStatus.COURIER_TO_CLIENT))))
+            .setPagination(Pagination.of(0, 1));
+        return deliveryService.search(request).getContent();
     }
 }
