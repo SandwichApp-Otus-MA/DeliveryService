94dbd6775586222218808155089b02b18fab3764
AnVoronov <AnVoronov@croc.ru>
2025-10-16 19:50:50 +0300

    Исправлена конфигурация

--- src/main/java/com/sandwich/app/schedule/DeliveryScheduler.java
+++ src/main/java/com/sandwich/app/schedule/DeliveryScheduler.java
@@ -3,9 +3,11 @@ package com.sandwich.app.schedule;
 import com.sandwich.app.service.CourierService;
 import com.sandwich.app.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DeliveryScheduler {
@@ -15,6 +17,8 @@ public class DeliveryScheduler {
 
     @Scheduled(initialDelay = 2000, fixedRate = 3000)
     public void processing() {
-        deliveryService.startSearchCourier().forEach(courierService::searchCourier);
+        var deliveries = deliveryService.startSearchCourier();
+        log.info("Подготовлено заказов: {} для поиска курьеров", deliveries.size());
+        deliveries.forEach(courierService::searchCourier);
     }
 }
